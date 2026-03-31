eb3a297c616d6a8c7a4c95e9c851dd24e2fb00d1
Avinash <buildwithavinash@users.noreply.github.com>
2026-03-31 23:13:45 +0530

    used Link to switch between pages faster

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
-import React from 'react'
+
 import HomePage from './pages/HomePage'
 import { Routes, Route } from 'react-router'
 import CheckoutPage from './pages/CheckoutPage'
+import OrdersPage from './pages/OrdersPage'
+import TrackingPage from './pages/TrackingPage'
 
 const App = () => {
   return (
     <Routes>
       <Route index element={<HomePage />} />
       <Route path='checkout' element={<CheckoutPage />} />
+      <Route path='orders' element={<OrdersPage />} />
+      <Route path='tracking' element={<TrackingPage />} />
     </Routes>
   )
 }
