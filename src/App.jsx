807458d927af5e84c874bcd7d175179048986b63
Avinash <buildwithavinash@users.noreply.github.com>
2026-04-01 22:45:17 +0530

    lifting the cart state to the parent

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-
+import axios from 'axios'
+import { useEffect, useState } from 'react'
 import HomePage from './pages/HomePage'
 import { Routes, Route } from 'react-router'
 import CheckoutPage from './pages/CheckoutPage'
@@ -6,10 +7,18 @@ import OrdersPage from './pages/OrdersPage'
 import TrackingPage from './pages/TrackingPage'
 
 const App = () => {
+  const [cart, setCart] = useState([]);
+
+  useEffect(()=> {
+    axios.get("/api/cart-items")
+  .then((response) => {
+    setCart(response.data);
+  })
+  }, [])
   return (
     <Routes>
-      <Route index element={<HomePage />} />
-      <Route path='checkout' element={<CheckoutPage />} />
+      <Route index element={<HomePage cart={cart} />} />
+      <Route path='checkout' element={<CheckoutPage cart={cart} />} />
       <Route path='orders' element={<OrdersPage />} />
       <Route path='tracking' element={<TrackingPage />} />
     </Routes>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,20 +3,17 @@ import Header from "../components/Header";
 import "./HomePage.css";
 import { useEffect, useState } from 'react';
 
-const HomePage = () => {
+const HomePage = ({ cart }) => {
   const [products, setProducts] = useState([]);
-  const [cart, setCart] = useState([]);
+  
 
   useEffect(()=>{
-    axios.get("http://localhost:3000/api/products")
+    axios.get("/api/products")
   .then((response) => {
     setProducts(response.data);
   });
 
-  axios.get("http://localhost:3000/api/cart-items")
-  .then((response) => {
-    setCart(response.data);
-  })
+  
   }, [])
   
 
