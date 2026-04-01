eaaf909f7c1b19afef1092aafb20a0fe101c1e97
Avinash <buildwithavinash@users.noreply.github.com>
2026-04-01 22:22:47 +0530

    data shown using backend

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,13 +1,18 @@
 import axios from 'axios';
 import Header from "../components/Header";
 import "./HomePage.css";
-import { products } from "../../starting-code/data/products";
+import { useEffect, useState } from 'react';
 
 const HomePage = () => {
-  axios.get("http://localhost:3000/api/products")
+  const [products, setProducts] = useState([]);
+
+  useEffect(()=>{
+    axios.get("http://localhost:3000/api/products")
   .then((response) => {
-    console.log(response.data);
+    setProducts(response.data);
   });
+  }, [])
+  
 
   return (
     <>
